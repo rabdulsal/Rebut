89e3423a13242900fb626c15228a16f673cdd9c6
Rashad Abdul-Salaam <rashad@getskinclick.com>
2017-03-20 23:31:39 -0500

    Record, Play & Display working

--- Rebut/RecordControlsViewController.swift
+++ Rebut/RecordControlsViewController.swift
@@ -8,22 +8,36 @@
 
 import Foundation
 import UIKit
+import IQAudioRecorderController
+import FDWaveformView
+
 
 class RecordControlsViewController : UIViewController {
     
     @IBOutlet weak var startBtn: UIButton!
     @IBOutlet weak var stopBtn: UIButton!
     @IBOutlet weak var recordingsTableView: UITableView!
+    @IBOutlet weak var recordingWaveForm: FDWaveformView!
     
     var allRecordings = [Recording]()
+    var allRecordingFiles = [String]()
     var recorderView: RebutRecordViewController!
+    var player = AVAudioPlayer() // TODO: Eventually move to ViewModel
     
+    // Rendering
+    fileprivate var startRendering = Date()
+    fileprivate var endRendering = Date()
+    fileprivate var startLoading = Date()
+    fileprivate var endLoading = Date()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         recordingsTableView.delegate = self
         recordingsTableView.dataSource = self
+        recordingWaveForm.delegate = self
         
+        // Make RecorderView
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         recorderView = storyboard.instantiateViewController(withIdentifier: "RecorderViewController") as! RebutRecordViewController
         recorderView.delegate = self
@@ -31,11 +45,19 @@ class RecordControlsViewController : UIViewController {
         //recorderView.view.backgroundColor = UIColor.green
         recorderView.modalTransitionStyle = .crossDissolve
         recorderView.modalPresentationStyle = UIModalPresentationStyle.overCurrentContext
+        
     }
     
     @IBAction func start() {
-        self.present(recorderView, animated: true, completion: nil)
-        recorderView.startRecording()
+        let controller = IQAudioRecorderViewController()
+        controller.delegate = self
+        controller.title = "Recorder"
+        controller.maximumRecordDuration = 10
+        controller.allowCropping = false
+        //    controller.barStyle = UIBarStyleDefault;
+        //    controller.normalTintColor = [UIColor magentaColor];
+        //    controller.highlightedTintColor = [UIColor orangeColor];
+        self.presentBlurredAudioRecorderViewControllerAnimated(controller)
     }
     
     @IBAction func play() {
@@ -62,12 +84,13 @@ extension RecordControlsViewController : RecorderViewDelegate {
 extension RecordControlsViewController : UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let recording = allRecordings[indexPath.row]
-        do {
-            try recording.play()
-        } catch {
-            print(error)
-        }
+        
+        let filePath = allRecordingFiles[indexPath.row]
+        let url = NSURL.fileURL(withPath: filePath)
+        
+        recordingWaveForm.audioURL = url
+        recordingWaveForm.progressSamples = self.recordingWaveForm.totalSamples / 2
+        play(url: url as NSURL)
     }
 }
 
@@ -76,13 +99,75 @@ extension RecordControlsViewController : UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "recordControlsIdentifier", for: indexPath) as! RecordingTableViewCell
 
-        let recording = allRecordings[indexPath.row]
-        cell.title.text = "\(recording.url)"
+        let recording = allRecordingFiles[indexPath.row]
+        cell.title.text = "\(recording)"
         
         return cell
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allRecordings.count > 0 ? allRecordings.count : 0
+        return allRecordingFiles.count > 0 ? allRecordingFiles.count : 0
+    }
+}
+
+// MARK: - IQAudioRecorderViewControllerDelegate
+
+extension RecordControlsViewController : IQAudioRecorderViewControllerDelegate {
+    func audioRecorderController(_ controller: IQAudioRecorderViewController, didFinishWithAudioAtPath filePath: String) {
+        allRecordingFiles.insert(filePath, at: 0)
+        print(filePath)
+        controller.dismiss(animated: true) { 
+            self.recordingsTableView.reloadData()
+        }
+    }
+    
+    func audioRecorderControllerDidCancel(_ controller: IQAudioRecorderViewController) {
+        
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK: - FDWaveformViewDelegate
+
+extension RecordControlsViewController: FDWaveformViewDelegate {
+    func waveformViewWillRender(_ waveformView: FDWaveformView) {
+        self.startRendering = Date()
+    }
+    
+    func waveformViewDidRender(_ waveformView: FDWaveformView) {
+        self.endRendering = Date()
+        NSLog("FDWaveformView rendering done, took %f seconds", self.endRendering.timeIntervalSince(self.startRendering))
+        UIView.animate(withDuration: 0.25, animations: {() -> Void in
+            waveformView.alpha = 1.0
+        })
+    }
+    
+    func waveformViewWillLoad(_ waveformView: FDWaveformView) {
+        self.startLoading = Date()
+    }
+    
+    func waveformViewDidLoad(_ waveformView: FDWaveformView) {
+        self.endLoading = Date()
+        NSLog("FDWaveformView loading done, took %f seconds", self.endLoading.timeIntervalSince(self.startLoading))
+    }
+}
+
+private extension RecordControlsViewController { // TODO: Eventually place in ViewModel
+    
+    func play(url:NSURL) {
+        print("playing \(url)")
+        
+        do {
+            self.player = try AVAudioPlayer(contentsOf: url as URL)
+            player.prepareToPlay()
+            player.volume = 1.0
+            player.play()
+        } catch let error as NSError {
+            //self.player = nil
+            print(error.localizedDescription)
+        } catch {
+            print("AVAudioPlayer init failed")
+        }
+        
     }
 }
